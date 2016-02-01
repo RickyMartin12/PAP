00d130ab4e9e36b289bc1465f2f88b76e4b18ece
RickyMartin12 <11knum15@gmail.com>
2016-02-01 23:29:45 +0000

    third commit

---
+++ sites/content/menu_rodada.php
@@ -0,0 +1,28 @@
+<header>
+<div class="container_12">
+    <div class="grid_12">
+           <h1><a href="index.php"><img src="img/title_blackjack.png" width="320" height="50"></a> </h1>
+        <div class="menu_block">
+               <nav>
+            <ul class="sf-menu">
+                   <li><a href="index.php">Home</a></li>
+                   <li><a href="pontuacao.php">Pontuação</a> </li>
+                   <li><a href="objectivos.php">Objectivos</a> </li>
+                   <li class="current"><a href="#">Rodada & Jogadas</a>
+                    <ul>
+                       <li class="current"><a href="rodada.php">Rodada</a></li>
+                       <li><a href="jogadas.php">Jogada </a></li>
+                    </ul>
+                   </li>
+                   <li><a href="videos.php">Videos</a></li>
+                   <li><a href="download.php">Download</a></li>
+                   <li><a href="blog.php">Blogue</a></li>
+                   <li><a href="contactos.php">Contactos</a></li>
+                 </ul>
+          </nav>
+               <div class="clear"></div>
+             </div>
+        
+         </div>
+  </div>
+</header>
